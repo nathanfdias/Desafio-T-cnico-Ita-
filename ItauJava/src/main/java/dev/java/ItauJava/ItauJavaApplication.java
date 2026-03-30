8bd07fc5377096e9dbc68aa295b487eacc923d91
nathanfdias <nathanfaria9@gmail.com>
2026-03-30 18:52:20 -0300

    Feat: Estrutura Desafio

---
+++ ItauJava/src/main/java/dev/java/ItauJava/ItauJavaApplication.java
@@ -0,0 +1,13 @@
+package dev.java.ItauJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItauJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItauJavaApplication.class, args);
+	}
+
+}
---
+++ ItauJava/src/test/java/dev/java/ItauJava/ItauJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java.ItauJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItauJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
